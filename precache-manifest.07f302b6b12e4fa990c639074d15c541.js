6231fa32bc7d4e03579e6dcdfc9cc43f9af47a07
politeperson <saul.rojas@ucsp.edu.pe>
2019-09-16 08:42:09 -0500

    GENERADOR DE CARATULAS LISTO

---
+++ precache-manifest.07f302b6b12e4fa990c639074d15c541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b132d1422dc4a7c42b7f94932e9641f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed416c765f60d0438415",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "a534c4cd93404df5dcb0",
+    "url": "/static/css/main.52acc9a1.chunk.css"
+  },
+  {
+    "revision": "ed416c765f60d0438415",
+    "url": "/static/js/2.b16bf456.chunk.js"
+  },
+  {
+    "revision": "a534c4cd93404df5dcb0",
+    "url": "/static/js/main.ce67d331.chunk.js"
+  },
+  {
+    "revision": "be884248f9829a7fde1f",
+    "url": "/static/js/runtime~main.13f93b10.js"
+  },
+  {
+    "revision": "e1b99e4188631cd3289bf9886871cea9",
+    "url": "/static/media/logo2.e1b99e41.png"
+  }
+]);
